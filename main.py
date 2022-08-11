11ddcd7f8866124a6fec794537725cf8d336eae6
ttrendsetter <klo4kov.99@gmail.com>
2022-08-11 11:40:21 +0300

    finished

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
 import pandas as pd
 import json
+import datetime
 import plotly.express as px
-res = requests.post('http://127.0.0.1:8000/data', json={}).text
+print("""Please input start date and end date format YYYY-MM-DD (from today for a week ahead)""")
+start_date = (input('input values or press enter if you want use default value: ')
+              or datetime.datetime.now().date())
+end_date = (input('input values or press enter if you want use default value: ')
+            or start_date + datetime.timedelta(days=7))
+req = {'start_date': str(start_date), 'end_date': str(end_date)}
+res = requests.post('http://127.0.0.1:8000/data', json=req).text
 res = json.loads(res)
 data = []
 for town in res:
@@ -12,6 +19,8 @@ df = pd.DataFrame(data, columns=['Город', 'Температура', 'Дат
 df = df.groupby('Город').mean()
 print(list(df.index))
 print(df['Температура'])
-fig = px.bar(data_frame=df, title='Средняя температура', y='Температура')
+
+fig = px.bar(data_frame=df,
+             title=f'Средняя температура c {start_date} по {end_date}', y='Температура')
 
 fig.show()
