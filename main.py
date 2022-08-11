c5def5624636711d71043ce791fd929b5d9b2f9c
ttrendsetter <klo4kov.99@gmail.com>
2022-08-11 11:29:30 +0300

    finished plot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 import json
-import plotly
+import plotly.express as px
 res = requests.post('http://127.0.0.1:8000/data', json={}).text
 res = json.loads(res)
 data = []
@@ -9,4 +9,9 @@ for town in res:
     for temp_data in res[town]:
         data.append((town, temp_data['temp'], temp_data['date']))
 df = pd.DataFrame(data, columns=['Город', 'Температура', 'Дата'])
-print(df.groupby('Город').mean())
+df = df.groupby('Город').mean()
+print(list(df.index))
+print(df['Температура'])
+fig = px.bar(data_frame=df, title='Средняя температура', y='Температура')
+
+fig.show()
